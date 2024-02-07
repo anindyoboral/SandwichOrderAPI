5cd4652676cc4c264175872c8ef87ca9d1036b08
jaouad00 <jajaouad@gmail.com>
2024-02-07 15:22:25 +0100

    Add CreateTable.sql

---
+++ src/main/java/CreateTable.sql
@@ -0,0 +1,9 @@
+CREATE table ABISPERSONS
+(
+    PERSONID NUMBER not null
+        primary key ,
+
+    FIRSTNAME VARCHAR2(50) not null,
+    LASTNAME VARCHAR2(50) not null
+)
+/
--- src/main/java/be/abis/sandwich/repository/DBPersonRepository.java
+++ src/main/java/be/abis/sandwich/repository/DBPersonRepository.java
@@ -22,20 +22,20 @@ public class DBPersonRepository implements PersonRepository{
 
     @Override
     public Person findPerson(int id) {
-        return jdbcTemplate.queryForObject("select * from abispersons where id=?",new PersonMapper(),id);
+        return jdbcTemplate.queryForObject("select * from abispersons where personId=?",new PersonMapper(),id);
     }
 
 
     @Override
     public void addPerson(Person p) throws IOException {
 
-        jdbcTemplate.update("insert into abispersons(id,firstname,lastname) values(?,?,?)",p.getPersonId(),p.getFirstName(),p.getLastName());
+        jdbcTemplate.update("insert into abispersons(personId,firstname,lastname) values(?,?,?)",p.getPersonId(),p.getFirstName(),p.getLastName());
 
     }
 
     @Override
     public void updatePerson(Person p) {
-        jdbcTemplate.update("update abispersons set id=?,firstname=?,lastname=? where id=?",p.getPersonId(),p.getFirstName(),p.getLastName());
+        jdbcTemplate.update("update abispersons set id=?,firstname=?,lastname=? where personId=?",p.getPersonId(),p.getFirstName(),p.getLastName());
     }
     @Override
     public void deletePerson(int id) {
@@ -47,7 +47,7 @@ public class DBPersonRepository implements PersonRepository{
     private static final class PersonMapper implements RowMapper<Person> {
         public Person mapRow(ResultSet rs, int rowNum) throws SQLException {
             Person person = new Person();
-            person.setPersonId(rs.getInt("id"));
+            person.setPersonId(rs.getInt("personId"));
             person.setFirstName(rs.getString("firstname"));
             person.setLastName(rs.getString("lastname"));
             return person;
