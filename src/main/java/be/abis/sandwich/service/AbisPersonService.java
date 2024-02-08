8de4c8c1e8007a4eee0c40510c061192403146d3
jaouad00 <jajaouad@gmail.com>
2024-02-08 09:16:15 +0100

    add service person methods

--- src/main/java/be/abis/sandwich/service/AbisPersonService.java
+++ src/main/java/be/abis/sandwich/service/AbisPersonService.java
@@ -3,10 +3,11 @@ package be.abis.sandwich.service;
 import be.abis.sandwich.model.Person;
 import be.abis.sandwich.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
-
+@Service
 public class AbisPersonService implements PersonService
 
 
