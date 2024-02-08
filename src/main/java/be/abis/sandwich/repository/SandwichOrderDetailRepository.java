7ff01b1586f221a0f740a577a2c808b9d189b8fc
anindyoboral <anindyoboral@gmail.com>
2024-02-08 11:49:46 +0100

    sandwich repo

--- src/main/java/be/abis/sandwich/repository/SandwichOrderDetailRepository.java
+++ src/main/java/be/abis/sandwich/repository/SandwichOrderDetailRepository.java
@@ -2,6 +2,11 @@ package be.abis.sandwich.repository;
 
 import be.abis.sandwich.model.SandwichOrderDetail;
 
+import java.util.List;
+
 public interface SandwichOrderDetailRepository {
     void addSandwichOrderDetail(SandwichOrderDetail sod);
+    List<SandwichOrderDetail> findSandwichorderDetailsById(int id);
+
+
 }
