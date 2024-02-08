d83c1cafca59edead4a22baf8a0ab424ae75fcf6
anindyoboral <anindyoboral@gmail.com>
2024-02-08 15:08:22 +0100

    msg

--- src/main/java/be/abis/sandwich/repository/DBCourseRepository.java
+++ src/main/java/be/abis/sandwich/repository/DBCourseRepository.java
@@ -4,13 +4,14 @@ import be.abis.sandwich.model.Course;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.List;
 
-@Repository
+@Component
 public class DBCourseRepository implements CourseRepository{
 
     @Autowired
--- src/test/java/be/abis/sandwich/service/AbisSandwichOrderServiceTest.java
+++ src/test/java/be/abis/sandwich/service/AbisSandwichOrderServiceTest.java
@@ -1,4 +1,26 @@
 package be.abis.sandwich.service;
 
+import be.abis.sandwich.model.Sandwich;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+@SpringBootTest
 public class AbisSandwichOrderServiceTest {
+
+    @Autowired
+    AbisSandwichOrderService abisSandwichOrderService;
+
+    @Test
+    void findAllSandwichesTest(){
+        List<Sandwich> allSandwiches = abisSandwichOrderService.findAllSandwiches();
+        System.out.println(allSandwiches.toString());
+    }
+
+
+
+
+
 }
