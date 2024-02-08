f444b9c6f7c7a89a88447d70caeb0eeb68442a39
brunoFromGroenland <youpikiller@hotmail.com>
2024-02-08 15:15:28 +0100

    added sql sources for order api

--- src/test/java/be/abis/sandwich/service/AbisSandwichOrderServiceTest.java
+++ src/test/java/be/abis/sandwich/service/AbisSandwichOrderServiceTest.java
@@ -1,4 +1,26 @@
 package be.abis.sandwich.service;
 
-public class AbisSandwichOrderServiceTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+@SpringBootTest
+class AbisSandwichOrderServiceTest {
+
+    @Autowired
+    SandwichOrderService sos;
+
+    @Test
+     void findAllSandwiches() {
+        assertTrue(sos.findAllSandwiches().size() > 0);
+    }
+
+    //void addSandwichOrderDetail(SandwichOrderDetail sod);
+    //void printSandwichOrder(SandwichOrder so);
+    //float calculateSandwichOrderPrice(SandwichOrder so);
+    //Sandwich findSandwichByName(String name);
+
+
 }
